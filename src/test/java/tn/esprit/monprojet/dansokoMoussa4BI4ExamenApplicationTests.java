0b628d6fe3eac300300ccb65b131d9b20732ff74
Dansoko22md <moussa.dansoko@esprit.tn>
2025-10-16 10:42:16 +0100

    sonar

--- src/test/java/tn/esprit/monprojet/dansokoMoussa4BI4ExamenApplicationTests.java
+++ src/test/java/tn/esprit/monprojet/dansokoMoussa4BI4ExamenApplicationTests.java
@@ -2,12 +2,15 @@ package tn.esprit.monprojet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Utilise application-test.yml
 class dansokoMoussa4BI4ExamenApplicationTests {
 
     @Test
     void contextLoads() {
+        // Test simple de chargement du contexte Spring
     }
 
 }
